e3183f552f07c91c05738e0a90c1722286fb332d
ibrahem ghanim <i.ghanim@likecard.com>
2024-08-15 15:21:48 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./core/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./core/auth/auth.component').then((m) => m.AuthComponent),
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./core/users/users.component').then((m) => m.UsersComponent),
+    children: [
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./core/user-component/user-component.component').then(
+            (m) => m.UserComponentComponent
+          ),
+      },
+      {
+        path: ':id/edit',
+        loadComponent: () =>
+          import(
+            './core/user-edit-component/user-edit-component.component'
+          ).then((m) => m.UserEditComponentComponent),
+      },
+    ],
+  },
+];
---
+++ src/app/core/user-edit-component/user-edit-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-edit-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-edit-component.component.html',
+  styleUrl: './user-edit-component.component.scss'
+})
+export class UserEditComponentComponent {
+
+}
